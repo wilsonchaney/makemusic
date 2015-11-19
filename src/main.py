a8b788170463dec07a61b783d36ce4432ade5c42
Wilson Chaney <wilsongchaney@gmail.com>
2015-11-19 10:31:55 -0600

    cleaned up main.py with comments/TODOs, and removed multiple run functionality

--- src/main.py
+++ src/main.py
@@ -1,77 +1,64 @@
 from Core import *
 from xml import MusicXMLWriter
-
 import time
-import numpy
 
+start_time = time.time()
 
-totalFactor = 0
-totalTime = 0
-numRuns = 1
-outputToXML = True
+song = Song.create_random_attributes(beats_per_measure=4,unique_sections=3,total_sections=6,key_signature="C")
+melody_engine = MelodyEngine(song)
 
-totalTime = 0
+total_repeat_count = 0
+while True:
+    repeat_needed = False
 
-for run in range(0,numRuns):
-    start = time.time()
+    # For each section, create the chord progression. (i.e. the chords for a verse, for a chorus, and so on.)
+    for section in song.get_unique_sections():
+        redo_count = 0
+        chords = get_chord_progression(song.key, song.num_chords_in_section(section))
 
-    song = Song.create_random_attributes(beats_per_measure=4,unique_sections=3,total_sections=6)
-    melody_engine = MelodyEngine(song)
-
-    total_repeat_count = 0
-    while True:
-        repeat_needed = False
-        for section in song.get_unique_sections():
-            redo_count = 0
+        # TODO: rework this. It's EXTREMELY inefficient.
+        # Currently, I'm re-doing the chord progression generation if I make two chord progressions that are too similar
+        # In chords.py, I need to add logic to ALWAYS generate a chord progression that is more "unique"
+        while len([prog for prog in song.get_all_chord_progressions() if not different_enough(chords, prog)]) > 0 and total_repeat_count < 50:
+            redo_count += 1
             chords = get_chord_progression(song.key, song.num_chords_in_section(section))
-
-            while len([prog for prog in song.get_all_chord_progressions() if not different_enough(chords, prog)]) > 0 and total_repeat_count < 50:
-                redo_count += 1
-                chords = get_chord_progression(song.key, song.num_chords_in_section(section))
-                if redo_count == 15:
-                    repeat_needed = True
-                    #print "Broke infinite loop."
-                    break # Restart all chord progresions
-            if repeat_needed:
-                break
-
-            song.set_chord_progression(section,chords)
+            if redo_count == 15:
+                repeat_needed = True
+                break # Restart all chord progresions
         if repeat_needed:
-            total_repeat_count += 1
-            continue
-        else:
             break
-    for section in song.get_unique_sections():
+        song.set_chord_progression(section,chords)
+    if repeat_needed:
+        total_repeat_count += 1
+        continue
+    else:
+        break
 
-        chords = song.get_chord_progression(section)
-        section_data = []
-        rhythmic_weight = random.randint(1,5)
-        song.set_rhythm_weight(section,rhythmic_weight)
-        #print section,rhythmic_weight
-        #raw_input()
-        melody = melody_engine.create_melody_beta(section)
-        num_beats_expected = song.num_measures_in_section(section)*song.beats_per_measure
-        num_beats_actual = sum([n.duration for n in melody])
-        song.set_section_melody(section,melody_engine.divide_cross_measure_notes(melody))
+# For each section, generate a melody (both pitches and rhythms)
+for section in song.get_unique_sections():
+    chords = song.get_chord_progression(section)
+    section_data = []
+    # See rhythm.py to understand this weight - it basically biases rhythm generation in favor of
+    # shorter notes or longer ones, depending on the value.
+    rhythmic_weight = random.randint(1,5)
+    song.set_rhythm_weight(section,rhythmic_weight)
+    melody = melody_engine.create_melody_beta(section)
+    num_beats_expected = song.num_measures_in_section(section)*song.beats_per_measure
+    num_beats_actual = sum([n.duration for n in melody])
+    song.set_section_melody(section,melody_engine.divide_cross_measure_notes(melody))
 
-    song.populate_measures()
+# TODO: document this.
+song.populate_measures()
 
-    last_pitch_before_final_measure = song.get_measures()[-1]._notes[-1].pitch
-    final_chord = Chord(1,"maj",song.key)
-    makeChordMeasure(final_chord,4)
-    song.append_final_measure(melody_engine.get_final_measure(song.beats_per_measure,final_chord,last_pitch_before_final_measure))
+last_pitch_before_final_measure = song.get_measures()[-1]._notes[-1].pitch
+final_chord = Chord(1,"maj",song.key)
+makeChordMeasure(final_chord,4)
+song.append_final_measure(melody_engine.get_final_measure(song.beats_per_measure,final_chord,last_pitch_before_final_measure))
 
-    mid = time.time()
-    writer = MusicXMLWriter(song)
-    writer.write("output.xml")
+writer = MusicXMLWriter(song)
+writer.write("output.xml")
 
-    end = time.time()
+end_time = time.time()
 
-    totalTime += 1000*(end-start)
-    for section in song.get_unique_sections():
-        pass
-        #print section,("measures:"+str(song.num_measures_in_section(section))),("chords:"+str(song.num_chords_in_section(section)))
-        #print song.get_rhythm_weight(section),"\n"
-    #print song.section_structure
-print "Execution time: ",totalTime/numRuns,"ms per song"
-print "Done"
+print "Done generating song."
+print "Time Elapsed:",str(1000*(end_time-start_time))+"ms"
